e521ca0e5a757480c327c992cedb57d70c37fbb6
tobinegbert <tobinegbert@gmail.com>
2014-09-15 08:46:55 -0600

    Mongo

--- JavaScript/nodejs.js
+++ JavaScript/nodejs.js
@@ -2,16 +2,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
 var htmlRoot = '/Users/tobinegbert/WebstormProjects/notes-app/HTML/';
 
 app.get('/', function(req, res){
-    res.sendFile(htmlRoot + 'index.html');
+    res.sendFile(htmlRoot + '/index.html');
 });
 
 app.get('/Math.html', function(req, res){
     res.sendFile(htmlRoot + 'Math.html');
+});
+
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+    if (err) {
+        return console.dir(err);
+    } else {
+        console.log("It worked!")
+    }
+    db.collection('test', function(err, collection) {
+        if (err) {
+            return console.log("Error ya idiot");
+        } else {
+            console.log('It worked!... Again')
+        }
+    });
 });
 
 io.on('connection', function(socket){
